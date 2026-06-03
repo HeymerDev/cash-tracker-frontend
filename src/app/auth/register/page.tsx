b185aa1659b5678c03a7f5562cfcf87e29224458
HeymerDev <heymermeza11@gmail.com>
2026-06-02 23:11:36 -0500

    moved register form to client component on file RegisterForm & add metada a register page

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,3 +1,25 @@
+import { RegisterFrom } from "@/components/auth/forms/RegisterFrom";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "CashTracker - Registrar",
+  description:
+    "Crea una nueva cuenta en CashTracker para controlar tus finanzas de manera eficiente.",
+  authors: [{ name: "HeymerDev" }],
+  keywords: [
+    "CashTracker",
+    "Registrar",
+    "Crear cuenta",
+    "Control de finanzas",
+    "Gestión financiera",
+    "Aplicación de finanzas",
+    "Seguimiento de gastos",
+    "Presupuesto personal",
+    "Finanzas personales",
+    "App de finanzas",
+  ],
+};
+
 const RegisterPage = () => {
   return (
     <>
@@ -6,57 +28,7 @@ const RegisterPage = () => {
         y controla tus <span className="text-amber-500">finanzas</span>
       </p>
 
-      <form className="mt-14 space-y-5" noValidate>
-        <div className="flex flex-col gap-2">
-          <label className="font-bold text-2xl" htmlFor="email">
-            Email
-          </label>
-          <input
-            id="email"
-            type="email"
-            placeholder="Email de Registro"
-            className="w-full border border-gray-300 p-3 rounded-lg"
-            name="email"
-          />
-        </div>
-
-        <div className="flex flex-col gap-2">
-          <label className="font-bold text-2xl">Nombre</label>
-          <input
-            type="name"
-            placeholder="Nombre de Registro"
-            className="w-full border border-gray-300 p-3 rounded-lg"
-            name="name"
-          />
-        </div>
-
-        <div className="flex flex-col gap-2">
-          <label className="font-bold text-2xl">Password</label>
-          <input
-            type="password"
-            placeholder="Password de Registro"
-            className="w-full border border-gray-300 p-3 rounded-lg"
-            name="password"
-          />
-        </div>
-
-        <div className="flex flex-col gap-2">
-          <label className="font-bold text-2xl">Repetir Password</label>
-          <input
-            id="password_confirmation"
-            type="password"
-            placeholder="Repite Password de Registro"
-            className="w-full border border-gray-300 p-3 rounded-lg"
-            name="password_confirmation"
-          />
-        </div>
-
-        <input
-          type="submit"
-          value="Registrarme"
-          className="bg-purple-950 hover:bg-purple-800 w-full p-3 rounded-lg text-white font-black  text-xl cursor-pointer block"
-        />
-      </form>
+      <RegisterFrom />
     </>
   );
 };

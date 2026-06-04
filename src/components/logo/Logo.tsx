df28a7677656d21dbc5d098eddac72d0f39bc5c7
HeymerDev <heymermeza11@gmail.com>
2026-06-03 22:26:14 -0500

    modified Logo.tsx component for it has priority and correct height

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -2,6 +2,12 @@ import Image from "next/image";
 
 export const Logo = () => {
   return (
-    <Image src="/logo.svg" alt="CashTracker Logo" width={150} height={150} />
+    <Image
+      src="/logo.svg"
+      alt="CashTracker Logo"
+      width={400}
+      height={123}
+      priority
+    />
   );
 };

ceab1ae202f36750f76b14f43788b6c84ee2873b
HeymerDev <heymermeza11@gmail.com>
2026-06-04 22:21:50 -0500

    add validation schema for validate register inputs

---
+++ src/schemas/auth/index.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const RegisterSchema = z
+  .object({
+    email: z.email("El email no es válido"),
+    name: z.string().min(2, "El nombre debe tener al menos 2 caracteres"),
+    password: z
+      .string()
+      .min(8, "La contraseña debe tener al menos 8 caracteres"),
+    password_confirmation: z.string(),
+  })
+  .refine((data) => data.password === data.password_confirmation, {
+    message: "Las contraseñas no coinciden",
+  });

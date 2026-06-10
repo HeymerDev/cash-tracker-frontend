9c12e7f9c59435bcd4f724d498994fb3876a0264
HeymerDev <heymermeza11@gmail.com>
2026-06-09 22:58:16 -0500

    should validtion errors on register form

--- src/components/auth/forms/RegisterFrom.tsx
+++ src/components/auth/forms/RegisterFrom.tsx
@@ -2,11 +2,17 @@
 
 import { useActionState } from "react";
 import { createAccount } from "@/actions/auth/register/create-account";
+import { ErrorMessage } from "../errors/ErrorMessage";
 
 export const RegisterFrom = () => {
   const [state, dispatch] = useActionState(createAccount, {
     errors: [],
   });
+
+  const hasError = (field: string) =>
+    state.errors.some((error) => error.path === field);
+
+  console.log(state);
 
   return (
     <form className="mt-14 space-y-5" noValidate action={dispatch}>
@@ -18,9 +24,22 @@ export const RegisterFrom = () => {
           id="email"
           type="email"
           placeholder="Email de Registro"
-          className="w-full border border-gray-300 p-3 rounded-lg"
+          className={`w-full p-3 rounded-lg border ${
+            hasError("email")
+              ? "border-red-500 placeholder:text-red-500"
+              : "border-gray-300"
+          }`}
           name="email"
         />
+
+        {state.errors
+          .filter((error) => error.path === "email")
+          .map((error) => (
+            <ErrorMessage
+              key={`${error.path}-${error.message}`}
+              message={error.message}
+            />
+          ))}
       </div>
 
       <div className="flex flex-col gap-2">
@@ -28,9 +47,22 @@ export const RegisterFrom = () => {
         <input
           type="name"
           placeholder="Nombre de Registro"
-          className="w-full border border-gray-300 p-3 rounded-lg"
+          className={`w-full p-3 rounded-lg border ${
+            hasError("name")
+              ? "border-red-500 placeholder:text-red-500"
+              : "border-gray-300"
+          }`}
           name="name"
         />
+
+        {state.errors
+          .filter((error) => error.path === "name")
+          .map((error) => (
+            <ErrorMessage
+              key={`${error.path}-${error.message}`}
+              message={error.message}
+            />
+          ))}
       </div>
 
       <div className="flex flex-col gap-2">
@@ -38,9 +70,22 @@ export const RegisterFrom = () => {
         <input
           type="password"
           placeholder="Password de Registro"
-          className="w-full border border-gray-300 p-3 rounded-lg"
+          className={`w-full p-3 rounded-lg border ${
+            hasError("password")
+              ? "border-red-500 placeholder:text-red-500"
+              : "border-gray-300"
+          }`}
           name="password"
         />
+
+        {state.errors
+          .filter((error) => error.path === "password")
+          .map((error) => (
+            <ErrorMessage
+              key={`${error.path}-${error.message}`}
+              message={error.message}
+            />
+          ))}
       </div>
 
       <div className="flex flex-col gap-2">
@@ -49,9 +94,22 @@ export const RegisterFrom = () => {
           id="password_confirmation"
           type="password"
           placeholder="Repite Password de Registro"
-          className="w-full border border-gray-300 p-3 rounded-lg"
+          className={`w-full p-3 rounded-lg border ${
+            hasError("password_confirmation")
+              ? "border-red-500 placeholder:text-red-500"
+              : "border-gray-300"
+          }`}
           name="password_confirmation"
         />
+
+        {state.errors
+          .filter((error) => error.path === "password_confirmation")
+          .map((error) => (
+            <ErrorMessage
+              key={`${error.path}-${error.message}`}
+              message={error.message}
+            />
+          ))}
       </div>
 
       <input

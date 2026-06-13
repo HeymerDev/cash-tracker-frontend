95cde6e741df26a88609b7597e9cdf91f6e3f855
HeymerDev <heymermeza11@gmail.com>
2026-06-12 21:54:09 -0500

    add confirm account otp component

---
+++ src/components/auth/inputs/OTPConfirmAccount.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { PinInput, PinInputField } from "@chakra-ui/pin-input";
+
+export const OTPConfirmAccount = () => {
+  return (
+    <div className="flex justify-center gap-5 my-10">
+      <PinInput>
+        <PinInputField className=" h-10 w-10 border border-gray-300 placeholder-white shadow rounded-lg text-center" />
+        <PinInputField className=" h-10 w-10 border border-gray-300 placeholder-white shadow rounded-lg text-center" />
+        <PinInputField className=" h-10 w-10 border border-gray-300 placeholder-white shadow rounded-lg text-center" />
+        <PinInputField className=" h-10 w-10 border border-gray-300 placeholder-white shadow rounded-lg text-center" />
+        <PinInputField className=" h-10 w-10 border border-gray-300 placeholder-white shadow rounded-lg text-center" />
+        <PinInputField className=" h-10 w-10 border border-gray-300 placeholder-white shadow rounded-lg text-center" />
+      </PinInput>
+    </div>
+  );
+};

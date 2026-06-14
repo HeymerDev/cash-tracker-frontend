e5083229b9d71060b33bd8f0ae7ec44570763658
HeymerDev <heymermeza11@gmail.com>
2026-06-13 22:19:40 -0500

    keep token on state for prepare to send on server action

--- src/components/auth/inputs/OTPConfirmAccount.tsx
+++ src/components/auth/inputs/OTPConfirmAccount.tsx
@@ -1,11 +1,18 @@
 "use client";
 
 import { PinInput, PinInputField } from "@chakra-ui/pin-input";
+import { useState } from "react";
 
 export const OTPConfirmAccount = () => {
+  const [token, setToken] = useState("");
+
+  const handleChange = (token: string) => {
+    setToken(token);
+  };
+
   return (
     <div className="flex justify-center gap-5 my-10">
-      <PinInput>
+      <PinInput value={token} onChange={handleChange}>
         <PinInputField className=" h-10 w-10 border border-gray-300 placeholder-white shadow rounded-lg text-center" />
         <PinInputField className=" h-10 w-10 border border-gray-300 placeholder-white shadow rounded-lg text-center" />
         <PinInputField className=" h-10 w-10 border border-gray-300 placeholder-white shadow rounded-lg text-center" />

19ec69c7348cadb6a7f38d0b166c9106deeabe50
HeymerDev <heymermeza11@gmail.com>
2026-06-26 22:06:42 -0500

    using Login response sche for iferent types LoginResponse

--- src/types/auth/login/index.ts
+++ src/types/auth/login/index.ts
@@ -1,11 +1,12 @@
+import { z } from "zod";
+import { LoginResponseSchema } from "@/schemas/auth";
+
 export type LoginError = {
   path: string;
   message: string;
 };
 
-export type LoginResponse = {
-  message: string;
-};
+export type LoginResponse = z.infer<typeof LoginResponseSchema>;
 
 export type LoginFields = {
   email: string;

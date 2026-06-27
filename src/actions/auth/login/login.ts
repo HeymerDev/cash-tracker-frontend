33008f912416c369a5e5e433d0d74a114a5b761c
HeymerDev <heymermeza11@gmail.com>
2026-06-26 22:39:21 -0500

    keep token on cookies

--- src/actions/auth/login/login.ts
+++ src/actions/auth/login/login.ts
@@ -2,8 +2,11 @@
 
 import { LoginResponseSchema, LoginSchema } from "@/schemas/auth";
 import { LoginResponse, LoginState } from "@/types/auth/login";
+import { cookies } from "next/headers";
 
 export const login = async (prevState: LoginState, formData: FormData) => {
+  const cookieStore = await cookies();
+
   const loginData = {
     email: formData.get("email") as string,
     password: formData.get("password") as string,
@@ -37,6 +40,15 @@ export const login = async (prevState: LoginState, formData: FormData) => {
 
     const json: LoginResponse = await request.json();
     const response = LoginResponseSchema.parse(json);
+
+    if (response.token) {
+      cookieStore.set({
+        name: "CASHTRACKER_TOKEN",
+        value: response.token,
+        httpOnly: true,
+        path: "/",
+      });
+    }
 
     return {
       errors: [],

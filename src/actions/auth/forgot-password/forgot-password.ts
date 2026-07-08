a50bf08327050af3e0952309f639de121b25d4bf
HeymerDev <heymermeza11@gmail.com>
2026-07-07 22:38:24 -0500

    modified forgot password for add send request to backend

--- src/actions/auth/forgot-password/forgot-password.ts
+++ src/actions/auth/forgot-password/forgot-password.ts
@@ -1,13 +1,12 @@
 "use server";
 
-import { ForgotPasswordSchema } from "@/schemas/auth";
+import { ForgotPasswordSchema, ResponseSchema } from "@/schemas/auth";
 import { ForgotPasswordState } from "@/types/auth/forgot-password";
 
 export const forgotPassword = async (
   prevState: ForgotPasswordState,
   formData: FormData,
 ) => {
-  console.log("FormData:", formData);
   const email = formData.get("email") as string;
 
   const forgotPassword = ForgotPasswordSchema.safeParse({ email });
@@ -19,8 +18,34 @@ export const forgotPassword = async (
     };
   }
 
-  return {
-    errors: [],
-    message: "Instrucciones enviadas al correo electrónico.",
-  };
+  try {
+    const req = await fetch(`${process.env.API_URL}/auth/forgot-password`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: forgotPassword.data.email,
+      }),
+    });
+
+    const json = await req.json();
+    const { message } = ResponseSchema.parse(json);
+
+    if (!req.ok) {
+      return { errors: [message], message: "" };
+    }
+
+    return {
+      message,
+      errors: [],
+    };
+  } catch (error) {
+    return {
+      errors: [
+        error instanceof Error ? error.message : "Ocurrió un error inesperado",
+      ],
+      message: "",
+    };
+  }
 };

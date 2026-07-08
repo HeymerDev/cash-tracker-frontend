1c44f053f2f3272e0c58f5ada542c3de56ade72f
HeymerDev <heymermeza11@gmail.com>
2026-07-07 23:00:54 -0500

    add validate password otp component

---
+++ src/components/auth/inputs/OTPValidatePasswordToken.tsx
@@ -0,0 +1,21 @@
+import { PinInput, PinInputField } from "@chakra-ui/pin-input";
+import React from "react";
+
+export const OTPValidatePasswordToken = () => {
+  const handleChange = (token: string) => {};
+
+  const handleComplete = () => {};
+
+  return (
+    <div className="flex justify-center gap-5 my-10">
+      <PinInput value={""} onChange={handleChange} onComplete={handleComplete}>
+        <PinInputField className="h-10 w-10 text-center border border-gray-300 shadow rounded-lg placeholder-white" />
+        <PinInputField className="h-10 w-10 text-center border border-gray-300 shadow rounded-lg placeholder-white" />
+        <PinInputField className="h-10 w-10 text-center border border-gray-300 shadow rounded-lg placeholder-white" />
+        <PinInputField className="h-10 w-10 text-center border border-gray-300 shadow rounded-lg placeholder-white" />
+        <PinInputField className="h-10 w-10 text-center border border-gray-300 shadow rounded-lg placeholder-white" />
+        <PinInputField className="h-10 w-10 text-center border border-gray-300 shadow rounded-lg placeholder-white" />
+      </PinInput>
+    </div>
+  );
+};

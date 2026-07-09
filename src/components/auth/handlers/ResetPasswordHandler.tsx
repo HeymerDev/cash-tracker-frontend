0327e72b1c985f15ee966baa9d6dfaff6cc8ee7d
HeymerDev <heymermeza11@gmail.com>
2026-07-08 22:17:21 -0500

    add reset password handler component

---
+++ src/components/auth/handlers/ResetPasswordHandler.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { useState } from "react";
+import { NewPasswordForm } from "../forms/NewPasswordForm";
+import { OTPValidatePasswordToken } from "../inputs/OTPValidatePasswordToken";
+
+export const ResetPasswordHandler = () => {
+  const [isValidToken, setIsValidToken] = useState<boolean>(false);
+  return (
+    <>{isValidToken ? <NewPasswordForm /> : <OTPValidatePasswordToken />}</>
+  );
+};

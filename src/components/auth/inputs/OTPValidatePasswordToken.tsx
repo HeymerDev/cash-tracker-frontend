d6b6837c47449ccdff1152ae045a8a87b2d457a6
HeymerDev <heymermeza11@gmail.com>
2026-07-09 22:56:46 -0500

    modified OTPValidatePasswordToken component fro use validate token password action

--- src/components/auth/inputs/OTPValidatePasswordToken.tsx
+++ src/components/auth/inputs/OTPValidatePasswordToken.tsx
@@ -1,21 +1,52 @@
+"use client";
+
+import { validateTokenPassword } from "@/actions/auth/new-password/validate-token-password";
 import { PinInput, PinInputField } from "@chakra-ui/pin-input";
-import React from "react";
+import { startTransition, useActionState, useMemo, useState } from "react";
 
 export const OTPValidatePasswordToken = () => {
-  const handleChange = (token: string) => {};
+  const [token, setToken] = useState<string>("");
 
-  const handleComplete = () => {};
+  const validateTokenAction = useMemo(
+    () => validateTokenPassword.bind(null, token),
+    [token],
+  );
+
+  const [state, dispatch, isPending] = useActionState(validateTokenAction, {
+    errors: [],
+    message: "",
+  });
+
+  const handleChange = (token: string) => {
+    setToken(token);
+  };
+
+  const handleComplete = () => {
+    startTransition(() => {
+      dispatch();
+    });
+  };
 
   return (
-    <div className="flex justify-center gap-5 my-10">
-      <PinInput value={""} onChange={handleChange} onComplete={handleComplete}>
-        <PinInputField className="h-10 w-10 text-center border border-gray-300 shadow rounded-lg placeholder-white" />
-        <PinInputField className="h-10 w-10 text-center border border-gray-300 shadow rounded-lg placeholder-white" />
-        <PinInputField className="h-10 w-10 text-center border border-gray-300 shadow rounded-lg placeholder-white" />
-        <PinInputField className="h-10 w-10 text-center border border-gray-300 shadow rounded-lg placeholder-white" />
-        <PinInputField className="h-10 w-10 text-center border border-gray-300 shadow rounded-lg placeholder-white" />
-        <PinInputField className="h-10 w-10 text-center border border-gray-300 shadow rounded-lg placeholder-white" />
-      </PinInput>
+    <div className="grid justify-center gap-5 my-10">
+      <div className="flex justify-center gap-5 my-10">
+        <PinInput value={token} onChange={handleChange}>
+          <PinInputField className="h-10 w-10 text-center border border-gray-300 shadow rounded-lg placeholder-white" />
+          <PinInputField className="h-10 w-10 text-center border border-gray-300 shadow rounded-lg placeholder-white" />
+          <PinInputField className="h-10 w-10 text-center border border-gray-300 shadow rounded-lg placeholder-white" />
+          <PinInputField className="h-10 w-10 text-center border border-gray-300 shadow rounded-lg placeholder-white" />
+          <PinInputField className="h-10 w-10 text-center border border-gray-300 shadow rounded-lg placeholder-white" />
+          <PinInputField className="h-10 w-10 text-center border border-gray-300 shadow rounded-lg placeholder-white" />
+        </PinInput>
+      </div>
+
+      <button
+        className="bg-purple-950 hover:bg-purple-800 w-full p-3 rounded-lg text-white font-black  text-xl cursor-pointer"
+        onClick={handleComplete}
+        disabled={isPending}
+      >
+        {isPending ? "Verificando..." : "Validar Código"}
+      </button>
     </div>
   );
 };

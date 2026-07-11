4e451a8c4a1c2036c020b03ac0f1cc151cb9d60b
HeymerDev <heymermeza11@gmail.com>
2026-07-10 22:13:24 -0500

    modified validate token password action for send request to backend

--- src/actions/auth/new-password/validate-token-password.ts
+++ src/actions/auth/new-password/validate-token-password.ts
@@ -1,12 +1,54 @@
 "use server";
 
 import { ValidateToeknPasswordState } from "@/types/auth/new-password";
+import { ResponseSchema, TokenSchema } from "@/schemas/auth/index";
 
 export const validateTokenPassword = async (
   token: string,
   prevState: ValidateToeknPasswordState,
 ) => {
-  console.log("Token:", token);
+  const resetPasswordToken = TokenSchema.safeParse({ token });
+
+  if (!resetPasswordToken.success) {
+    return {
+      errors: resetPasswordToken.error.issues.map((error) => error.message),
+      message: "",
+    };
+  }
+
+  try {
+    const request = await fetch(
+      `${process.env.API_URL}/auth/validate-reset-token`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          token: resetPasswordToken.data.token,
+        }),
+      },
+    );
+
+    const json = await request.json();
+    const { message } = ResponseSchema.parse(json);
+
+    if (!request.ok) {
+      return { errors: [message], message: "" };
+    }
+
+    return {
+      message,
+      errors: [],
+    };
+  } catch (error) {
+    return {
+      errors: [
+        error instanceof Error ? error.message : "Ocurrió un error inesperado",
+      ],
+      message: "",
+    };
+  }
 
   return {
     errors: [],

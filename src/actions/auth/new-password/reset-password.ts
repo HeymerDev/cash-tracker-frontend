39aeec0ab0e9751b25792800410486b59aeb4081
HeymerDev <heymermeza11@gmail.com>
2026-07-12 21:25:26 -0500

    add new password action and use validation schema

---
+++ src/actions/auth/new-password/reset-password.ts
@@ -0,0 +1,36 @@
+"use server";
+
+import { ResetPasswordSchema } from "@/schemas/auth";
+import { NewPasswordState } from "@/types/auth/new-password";
+
+export const resetPassword = async (
+  prevState: NewPasswordState,
+  formData: FormData,
+) => {
+  const password = formData.get("password") as string;
+  const password_confirmation = formData.get("password_confirmation") as string;
+
+  const resetPassword = ResetPasswordSchema.safeParse({
+    password,
+    password_confirmation,
+  });
+
+  if (!resetPassword.success) {
+    return {
+      errors: resetPassword.error.issues.map((error) => ({
+        path: String(error.path[0]),
+        message: error.message,
+      })),
+      fields: {
+        password,
+        password_confirmation,
+      },
+    };
+  }
+
+  return {
+    errors: [],
+    fields: { password: "", password_confirmation: "" },
+    message: "Password reset successfully",
+  };
+};

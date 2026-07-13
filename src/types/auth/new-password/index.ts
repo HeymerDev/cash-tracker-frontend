f9365a7fc884f45d8e78793a7ba8edfed7dfcc01
HeymerDev <heymermeza11@gmail.com>
2026-07-12 21:20:52 -0500

    modified new-password types for add new password form action state types

--- src/types/auth/new-password/index.ts
+++ src/types/auth/new-password/index.ts
@@ -2,3 +2,19 @@ export type ValidateToeknPasswordState = {
   errors: string[];
   message: string;
 };
+
+export type NewPasswordError = {
+  path: string;
+  message: string;
+};
+export type NewPasswordFields = {
+  password: string;
+  password_confirmation: string;
+};
+
+export type NewPasswordState = {
+  errors: NewPasswordError[];
+  fields: NewPasswordFields;
+  message?: string;
+  status?: number;
+};

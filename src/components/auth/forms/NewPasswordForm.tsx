e117463ce24c984a110fcbb75b94bf22a36969e6
HeymerDev <heymermeza11@gmail.com>
2026-07-12 22:20:58 -0500

    modified NewPasswordForm.tsx for show api response on toast and navigate to login if request is succes

--- src/components/auth/forms/NewPasswordForm.tsx
+++ src/components/auth/forms/NewPasswordForm.tsx
@@ -1,11 +1,15 @@
 import { resetPassword } from "@/actions/auth/new-password/reset-password";
 import { FormField } from "../inputs/FormField";
-import { useActionState } from "react";
+import { useActionState, useEffect } from "react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 interface Props {
   token: string;
 }
 export const NewPasswordForm = ({ token }: Props) => {
+  const router = useRouter();
+
   const resetPasswordAction = resetPassword.bind(null, token);
 
   const [state, dispatch, pending] = useActionState(resetPasswordAction, {
@@ -13,6 +17,23 @@ export const NewPasswordForm = ({ token }: Props) => {
     fields: { password: "", password_confirmation: "" },
     message: "",
   });
+
+  useEffect(() => {
+    if (!state.message) return;
+
+    if (state.status === 200) {
+      toast.success(state.message, {
+        onDismiss: () => {
+          router.push("/auth/login");
+        },
+        onAutoClose: () => {
+          router.push("/auth/login");
+        },
+      });
+    } else {
+      toast.error(state.message);
+    }
+  }, [state, router]);
 
   return (
     <form className=" mt-14 space-y-5" noValidate action={dispatch}>
@@ -36,7 +57,7 @@ export const NewPasswordForm = ({ token }: Props) => {
 
       <input
         type="submit"
-        value="Guardar Password"
+        value={pending ? "Cambiando Password" : "Cambiar Password"}
         className="bg-purple-950 hover:bg-purple-800 w-full p-3 rounded-lg text-white font-black  text-xl cursor-pointer block"
       />
     </form>

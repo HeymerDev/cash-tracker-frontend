a7f6dab9aafa8eb6bc627144b0bab91b4841b4b1
HeymerDev <heymermeza11@gmail.com>
2026-07-14 22:19:31 -0500

    add create budget validation schema

---
+++ src/schemas/admin/budget.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const CreateBudgetSchema = z.object({
+  name: z
+    .string()
+    .min(1, { message: "El Nombre del presupuesto es obligatorio" }),
+  amount: z.coerce
+    .number({ message: "Cantidad no válida" })
+    .min(1, { message: "Cantidad no válida" }),
+});

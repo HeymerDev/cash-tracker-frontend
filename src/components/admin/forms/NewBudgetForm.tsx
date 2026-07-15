c696e6ef6c270312fa3125db2531bf5b69c8f1fe
HeymerDev <heymermeza11@gmail.com>
2026-07-14 22:21:31 -0500

    modified new budget form for show inputs error of validation

--- src/components/admin/forms/NewBudgetForm.tsx
+++ src/components/admin/forms/NewBudgetForm.tsx
@@ -1,28 +1,38 @@
 "use client";
 
+import { createBudget } from "@/actions/admin/budget/create-budget";
 import { FormField } from "@/components/auth/inputs/FormField";
+import { useActionState } from "react";
 
 export function CreateBudgetForm() {
+  const [state, dispatch, isPending] = useActionState(createBudget, {
+    errors: [],
+    message: "",
+    fields: { name: "", amount: 0 },
+  });
   return (
-    <form className="mt-10 space-y-3" noValidate>
+    <form className="mt-10 space-y-3" noValidate action={dispatch}>
       <FormField
-        errors={[]}
+        errors={state.errors}
         name="name"
         placeholder="Nombre del presupuesto"
         label="Nombre del presupuesto"
+        defaultValue={state.fields.name}
       />
 
       <FormField
-        errors={[]}
+        errors={state.errors}
         name="amount"
         placeholder="Cantidad Presupuesto"
         label="Cantidad Presupuesto"
         type="number"
+        defaultValue={state.fields.amount}
       />
       <input
+        disabled={isPending}
         type="submit"
         className="bg-amber-500 w-full p-3 text-white uppercase font-bold hover:bg-amber-600 cursor-pointer transition-colors"
-        value="Crear Presupuesto"
+        value={isPending ? "Creando Presupuesto" : "Crear Presupuesto"}
       />
     </form>
   );

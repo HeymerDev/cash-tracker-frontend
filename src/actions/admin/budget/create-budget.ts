bbc72e537e0a95c9135df7b22ed22dfeec1c6b5e
HeymerDev <heymermeza11@gmail.com>
2026-07-14 22:47:52 -0500

    modified create budget action for sendid request to api

--- src/actions/admin/budget/create-budget.ts
+++ src/actions/admin/budget/create-budget.ts
@@ -1,12 +1,17 @@
 "use server";
 
 import { CreateBudgetSchema } from "@/schemas/admin/budget";
+import { ResponseSchema } from "@/schemas/auth";
 import { CreateBudgetState } from "@/types/admin/budget";
+import { cookies } from "next/headers";
 
 export const createBudget = async (
   prevState: CreateBudgetState,
   formData: FormData,
-) => {
+): Promise<CreateBudgetState> => {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("CASHTRACKER_TOKEN")?.value;
+
   const name = formData.get("name") as string;
   const amount = parseFloat(formData.get("amount") as string);
 
@@ -29,11 +34,53 @@ export const createBudget = async (
     };
   }
 
-  console.log(budget.data);
+  try {
+    const request = await fetch(`${process.env.API_URL}/budgets`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        name: budget.data.name,
+        amount: budget.data.amount,
+      }),
+    });
 
-  return {
-    errors: [],
-    fields: { name: "", amount: 0 },
-    message: "",
-  };
+    const json = await request.json();
+
+    console.log(json);
+
+    const { message } = ResponseSchema.parse(json);
+
+    if (!request.ok) {
+      return {
+        errors: [],
+        fields: {
+          name,
+          amount,
+        },
+        message: message,
+        status: request.status,
+      };
+    }
+
+    return {
+      errors: [],
+      fields: { name: "", amount: 0 },
+      message: message,
+      status: request.status,
+    };
+  } catch (error) {
+    console.log("Error:", error); // Log the error to see what went wrong
+    return {
+      errors: [],
+      fields: {
+        name,
+        amount,
+      },
+      message: "Error interno del servidor",
+      status: 500,
+    };
+  }
 };

accc39d2161708514f5406122185cb371da3bd0b
HeymerDev <heymermeza11@gmail.com>
2026-07-16 21:17:25 -0500

    add format currency utility

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,10 @@ import { RegisterError } from "@/types/auth/register";
 export const hasError = (errors: RegisterError[], field: string) => {
   return errors.some((error) => error.path === field);
 };
+
+export const formatCurrency = (amount: number) => {
+  return new Intl.NumberFormat("es-CO", {
+    style: "currency",
+    currency: "COP",
+  }).format(amount);
+};

d709e15be80053b510e368ddadec5b0e75192dfa
HeymerDev <heymermeza11@gmail.com>
2026-07-22 22:04:56 -0500

    add api validation error type

--- src/types/admin/index.ts
+++ src/types/admin/index.ts
@@ -1,4 +1,9 @@
+import { ApiValidationErrorsResponseSchema } from "@/schemas";
 import { UserSchema } from "@/schemas/auth";
 import { z } from "zod";
 
 export type User = z.infer<typeof UserSchema>;
+
+export type ApiValidationError = z.infer<
+  typeof ApiValidationErrorsResponseSchema
+>;

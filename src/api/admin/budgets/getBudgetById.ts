472b2ed38fa62e931baa8f4965f6855507f62dab
HeymerDev <heymermeza11@gmail.com>
2026-07-23 21:27:11 -0500

    fix getBudgetById for not found error

--- src/api/admin/budgets/getBudgetById.ts
+++ src/api/admin/budgets/getBudgetById.ts
@@ -1,33 +1,26 @@
 import { getToken } from "@/dal/token";
-import { ApiValidationErrorsResponseSchema } from "@/schemas";
 import { BudgetSchema } from "@/schemas/admin/budget";
-import { ApiValidationError } from "@/types/admin";
-import { Budget } from "@/types/admin/budget";
+import { notFound } from "next/navigation";
 
 export const getBudgetById = async (id: string) => {
   const token = await getToken();
 
+  const request = await fetch(`${process.env.API_URL}/budgets/${id}`, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (!request.ok) {
+    notFound();
+  }
+
   try {
-    const request = await fetch(`${process.env.API_URL}/budgets/${id}`, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    });
-
     const json = await request.json();
-
-    if (!request.ok) {
-      const validationError: ApiValidationError =
-        ApiValidationErrorsResponseSchema.parse(json);
-
-      return validationError.errors[0].msg;
-    }
-
-    const budget: Budget = BudgetSchema.parse(json);
-
-    return budget;
+    return BudgetSchema.parse(json);
   } catch (error) {
-    console.log(error);
+    console.error(error);
+    throw new Error("Error procesando la respuesta");
   }
 };

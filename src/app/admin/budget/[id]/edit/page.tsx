d6f0f121bd1e87f15abcd9a1ba0c99720188dc47
HeymerDev <heymermeza11@gmail.com>
2026-07-23 22:19:24 -0500

    modified edit budget page for add UI & use EditBudgetForm.tsx

--- src/app/admin/budget/[id]/edit/page.tsx
+++ src/app/admin/budget/[id]/edit/page.tsx
@@ -1,4 +1,6 @@
 import { getBudgetById } from "@/api/admin/budgets/getBudgetById";
+import { EditBudgetForm } from "@/components/admin/forms/EditBudgetForm";
+import Link from "next/link";
 
 const EditBudgetPage = async ({ params }: { params: { id: string } }) => {
   const { id } = await params;
@@ -6,9 +8,30 @@ const EditBudgetPage = async ({ params }: { params: { id: string } }) => {
   const budget = await getBudgetById(id);
 
   return (
-    <div>
-      EditBudgetPage <pre>{JSON.stringify(budget, null, 2)}</pre>
-    </div>
+    <>
+      <div className="flex flex-col-reverse md:flex-row md:justify-between items-center">
+        <div className="w-full md:w-auto">
+          <h1 className="font-black text-4xl text-purple-950 my-5">
+            Editar Presupuesto
+            <br />
+            <span className="text-2xl">{budget.name}</span>
+          </h1>
+          <p className="text-xl font-bold">
+            Llena el formulario y crea un nuevo {""}
+            <span className="text-amber-500">presupuesto</span>
+          </p>
+        </div>
+        <Link
+          href={"/admin"}
+          className="bg-amber-500 p-2 rounded-lg text-white font-bold w-full md:w-auto text-center"
+        >
+          Volver
+        </Link>
+      </div>
+      <div className="p-10 mt-10 shadow-lg">
+        <EditBudgetForm budget={budget} />
+      </div>
+    </>
   );
 };
 

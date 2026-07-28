91e92ccb80e44a9bd0222efcd315ef60712941c6
HeymerDev <heymermeza11@gmail.com>
2026-07-27 21:42:23 -0500

    modified edit-budget action to add cache

--- src/actions/admin/budget/edit-budget.ts
+++ src/actions/admin/budget/edit-budget.ts
@@ -5,84 +5,87 @@ import { EditBudgetSchema } from "@/schemas/admin/budget";
 import { ResponseSchema } from "@/schemas/auth";
 import { Budget, EditBudgetState } from "@/types/admin/budget";
 import { revalidatePath } from "next/cache";
+import { cache } from "react";
 
-export const editBudget = async (
-  id: Budget["id"],
-  prevState: EditBudgetState,
-  formData: FormData,
-): Promise<EditBudgetState> => {
-  const token = await getToken();
-  const name = formData.get("name") as string;
-  const amount = formData.get("amount") as string;
+export const editBudget = cache(
+  async (
+    id: Budget["id"],
+    prevState: EditBudgetState,
+    formData: FormData,
+  ): Promise<EditBudgetState> => {
+    const token = await getToken();
+    const name = formData.get("name") as string;
+    const amount = formData.get("amount") as string;
 
-  const budget = EditBudgetSchema.safeParse({
-    name,
-    amount,
-  });
-
-  if (!budget.success) {
-    return {
-      errors: budget.error.issues.map((error) => ({
-        path: String(error.path[0]),
-        message: error.message,
-      })),
-      fields: {
-        name,
-        amount: Number(amount),
-      },
-      message: "",
-    };
-  }
-
-  try {
-    const request = await fetch(`${process.env.API_URL}/budgets/${id}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify({
-        name: budget.data.name,
-        amount: budget.data.amount,
-      }),
+    const budget = EditBudgetSchema.safeParse({
+      name,
+      amount,
     });
 
-    const json = await request.json();
+    if (!budget.success) {
+      return {
+        errors: budget.error.issues.map((error) => ({
+          path: String(error.path[0]),
+          message: error.message,
+        })),
+        fields: {
+          name,
+          amount: Number(amount),
+        },
+        message: "",
+      };
+    }
 
-    console.log(json);
+    try {
+      const request = await fetch(`${process.env.API_URL}/budgets/${id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          name: budget.data.name,
+          amount: budget.data.amount,
+        }),
+      });
 
-    const { message } = ResponseSchema.parse(json);
+      const json = await request.json();
 
-    if (!request.ok) {
+      console.log(json);
+
+      const { message } = ResponseSchema.parse(json);
+
+      if (!request.ok) {
+        return {
+          errors: [],
+          fields: {
+            name,
+            amount: Number(amount),
+          },
+          message: message,
+          status: request.status,
+        };
+      }
+
+      revalidatePath("/admin");
+
+      return {
+        errors: [],
+        fields: { name: "", amount: 0 },
+        message: message,
+        status: request.status,
+      };
+    } catch (error) {
+      console.log("Error:", error); // Log the error to see what went wrong
       return {
         errors: [],
         fields: {
           name,
           amount: Number(amount),
         },
-        message: message,
-        status: request.status,
+        message: "Error interno del servidor",
+        status: 500,
       };
     }
-
-    revalidatePath("/admin");
-
-    return {
-      errors: [],
-      fields: { name: "", amount: 0 },
-      message: message,
-      status: request.status,
-    };
-  } catch (error) {
-    console.log("Error:", error); // Log the error to see what went wrong
-    return {
-      errors: [],
-      fields: {
-        name,
-        amount: Number(amount),
-      },
-      message: "Error interno del servidor",
-      status: 500,
-    };
-  }
-};
+  },
+);

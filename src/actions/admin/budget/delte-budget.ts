564b8a3d4965471844de768c43530914f2fd4e3c
HeymerDev <heymermeza11@gmail.com>
2026-07-31 21:56:45 -0500

    modified delete budget action for send request to api check password

--- src/actions/admin/budget/delte-budget.ts
+++ src/actions/admin/budget/delte-budget.ts
@@ -1,6 +1,8 @@
 "use server";
 
+import { getToken } from "@/dal/token";
 import { PasswordValidationSchema } from "@/schemas/admin/budget";
+import { ResponseSchema } from "@/schemas/auth";
 import { Budget, DeletBudgetState } from "@/types/admin/budget";
 
 export const deleteBudget = async (
@@ -8,6 +10,8 @@ export const deleteBudget = async (
   prevState: DeletBudgetState,
   formData: FormData,
 ) => {
+  const token = await getToken();
+
   const password = formData.get("password") as string;
 
   const validatePassword = PasswordValidationSchema.safeParse(password);
@@ -23,6 +27,43 @@ export const deleteBudget = async (
         password: password,
       },
       message: "",
+    };
+  }
+
+  try {
+    const request = await fetch(`${process.env.API_URL}/auth/check-password`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        password: validatePassword.data,
+      }),
+    });
+
+    const json = await request.json();
+
+    const { message } = ResponseSchema.parse(json);
+
+    if (!request.ok) {
+      return {
+        errors: [],
+        fields: {
+          password: password,
+        },
+        message: message,
+        status: request.status,
+      };
+    }
+  } catch (error) {
+    console.error("Error checking password:", error);
+    return {
+      errors: [],
+      fields: {
+        password: password,
+      },
+      message: "An error occurred while checking the password.",
     };
   }
 

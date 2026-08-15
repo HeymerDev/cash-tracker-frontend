2088a3f827a3af0266e4b0e07a137829a0b8cc40
HeymerDev <heymermeza11@gmail.com>
2026-08-14 22:19:20 -0500

    modified busget/[id]/page.tsx for use expense list component

--- src/app/admin/budget/[id]/page.tsx
+++ src/app/admin/budget/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { getBudgetById } from "@/api/admin/budgets/getBudgetById";
 import { AddExpenseButton } from "@/components/admin/buttons/AddExpenseButton";
 import { GenericDialog } from "@/components/admin/dialogs/GenericDialog";
-import { formatCurrency, formatDate } from "@/utils";
+import { ExpenseLists } from "@/components/admin/list/ExpenseLists";
 
 export async function generateMetadata({
   params,
@@ -30,39 +30,7 @@ const BudgetPage = async ({ params }: { params: { id: string } }) => {
         <AddExpenseButton />
       </div>
 
-      {budget.expenses.length > 0 ? (
-        <>
-          <h1 className="text-2xl font-bold text-purple-950 mt-10">Gastos</h1>
-          <ul role="list" className="divide-y divide-gray-300 shadow-lg mt-10 ">
-            {budget.expenses.map((expense) => (
-              <li key={expense.id} className="flex justify-between gap-x-6 p-5">
-                <div className="flex min-w-0 gap-x-4">
-                  <div className="min-w-0 flex-auto space-y-2">
-                    <p className="text-2xl font-semibold text-gray-900">
-                      {expense.name}
-                    </p>
-                    <p className="text-xl font-bold text-amber-500">
-                      {formatCurrency(expense.amount)}
-                    </p>
-                    <p className="text-gray-500  text-sm">
-                      Agregado el{" "}
-                      <span className="font-bold">
-                        {formatDate(expense.createdAt)}
-                      </span>
-                    </p>
-                  </div>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </>
-      ) : (
-        <div className="mt-10">
-          <p className="text-gray-500 text-lg">
-            No hay gastos registrados para este presupuesto.
-          </p>
-        </div>
-      )}
+      <ExpenseLists expenses={budget.expenses} />
 
       <GenericDialog />
     </>

418764426fb623d9dc1bd51edecc8a5c8809624f
HeymerDev <heymermeza11@gmail.com>
2026-08-21 21:58:06 -0500

    add send request to espense on handler

--- src/app/handlers/budget/[budgetId]/expenses/[expenseId]/route.ts
+++ src/app/handlers/budget/[budgetId]/expenses/[expenseId]/route.ts
@@ -1,11 +1,39 @@
 import { verifySession } from "@/dal/auth";
+import { getToken } from "@/dal/token";
+import { ExpenseSchema } from "@/schemas/admin/expense";
+import { ResponseSchema } from "@/schemas/auth";
 
 export async function GET(
   request: Request,
   { params }: { params: { budgetId: string; expenseId: string } },
 ) {
   await verifySession();
+  const token = await getToken();
   const { budgetId, expenseId } = await params;
 
-  return Response.json({ budgetId, expenseId });
+  try {
+    const req = await fetch(
+      `${process.env.API_URL}/budgets/${budgetId}/expenses/${expenseId}`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    );
+
+    const json = await req.json();
+
+    if (!req.ok) {
+      const { message } = ResponseSchema.parse(json);
+      return Response.json({ message }, { status: req.status });
+    }
+
+    const expense = ExpenseSchema.parse(json);
+
+    return Response.json(expense, { status: req.status });
+  } catch (error) {
+    console.log(error);
+    return Response.json({ error: "Failed to fetch expense" }, { status: 500 });
+  }
 }

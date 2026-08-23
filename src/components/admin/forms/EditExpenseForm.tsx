f77f4f562a719a390134f31bbf21c6c98f16d9cc
HeymerDev <heymermeza11@gmail.com>
2026-08-22 23:24:04 -0500

    modified EditExpenseForm.tsx for send server action & show api messages and errors

--- src/components/admin/forms/EditExpenseForm.tsx
+++ src/components/admin/forms/EditExpenseForm.tsx
@@ -1,17 +1,46 @@
+import { editExpense } from "@/actions/admin/expense/edit-expense";
 import { getExpenseById } from "@/api/admin/expenses/getExpenseById";
 import { FormField } from "@/components/auth/inputs/FormField";
 import { Expense } from "@/types/admin/expense";
 import { DialogTitle } from "@headlessui/react";
-import { useParams, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useParams, useRouter, useSearchParams } from "next/navigation";
+import { useActionState, useEffect, useState } from "react";
+import { toast } from "sonner";
 
-export function EditExpenseForm({ closeModal }: { closeModal: () => void }) {
+export function EditExpenseForm() {
   const [expense, setExpense] = useState<Pick<Expense, "name" | "amount">>();
 
   const { id: budgetId } = useParams();
   const searchParams = useSearchParams();
+  const router = useRouter();
 
   const expenseId = searchParams.get("expenseId") as string;
+
+  const editExpenseAction = editExpense.bind(
+    null,
+    Number(budgetId),
+    Number(expenseId),
+  );
+
+  const [state, dispatch, pending] = useActionState(editExpenseAction, {
+    errors: [],
+    fields: {
+      name: expense?.name || "",
+      amount: expense?.amount || 0,
+    },
+    message: "",
+  });
+
+  useEffect(() => {
+    if (!state.message) return;
+
+    if (state.status === 200) {
+      toast.success(state.message);
+      router.push(`/admin/budget/${budgetId}`);
+    } else {
+      toast.error(state.message);
+    }
+  }, [state, router, budgetId]);
 
   useEffect(() => {
     const fetchExpense = async () => {
@@ -36,27 +65,28 @@ export function EditExpenseForm({ closeModal }: { closeModal: () => void }) {
       <form
         className="bg-gray-100 shadow-lg rounded-lg p-10 mt-10 grid gap-9"
         noValidate
+        action={dispatch}
       >
         <FormField
-          errors={[]}
+          errors={state.errors}
           name="name"
           placeholder="Nombre del gasto"
           label="Nombre del gasto"
-          defaultValue={expense?.name}
+          defaultValue={state.fields.name || expense?.name}
         />
 
         <FormField
-          errors={[]}
+          errors={state.errors}
           name="amount"
           placeholder="Cantidad del gasto"
           label="Cantidad del gasto"
           type="number"
-          defaultValue={expense?.amount}
+          defaultValue={state.fields.amount || expense?.amount}
         />
         <input
           type="submit"
           className="bg-amber-500 w-full p-3 text-white uppercase font-bold hover:bg-amber-600 cursor-pointer transition-colors"
-          value="Guardar Cambios"
+          value={pending ? "Editando Gasto" : "Editar Gasto"}
         />
       </form>
     </>

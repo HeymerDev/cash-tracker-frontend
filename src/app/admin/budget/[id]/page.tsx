ae8663b96043bec58672bc7a52c06302c336d559
HeymerDev <heymermeza11@gmail.com>
2026-08-26 22:22:12 -0500

    modified budget id page for calculated available, spent and amount budget & show amount component with tis data

--- src/app/admin/budget/[id]/page.tsx
+++ src/app/admin/budget/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { getBudgetById } from "@/api/admin/budgets/getBudgetById";
 import { AddExpenseButton } from "@/components/admin/buttons/AddExpenseButton";
 import { GenericDialog } from "@/components/admin/dialogs/GenericDialog";
+import { Amount } from "@/components/admin/list/Amount";
 import { ExpenseLists } from "@/components/admin/list/ExpenseLists";
 
 export async function generateMetadata({
@@ -18,6 +19,13 @@ const BudgetPage = async ({ params }: { params: { id: string } }) => {
 
   const budget = await getBudgetById(id);
 
+  const totalSpent = budget.expenses.reduce(
+    (total, expense) => expense.amount + total,
+    0,
+  );
+
+  const totalAvailable = Number(budget.amount) - totalSpent;
+
   return (
     <>
       <div className="flex justify-between items-center">
@@ -28,6 +36,15 @@ const BudgetPage = async ({ params }: { params: { id: string } }) => {
           </p>
         </div>
         <AddExpenseButton />
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 mt-10">
+        <div>Grafica</div>
+        <div className="flex flex-col justify-center items-center md:items-start gap-5">
+          <Amount amount={Number(budget.amount)} label="Presupesto" />
+          <Amount amount={totalSpent} label="Gastado" />
+          <Amount amount={totalAvailable} label="Disponible" />
+        </div>
       </div>
 
       <ExpenseLists expenses={budget.expenses} />

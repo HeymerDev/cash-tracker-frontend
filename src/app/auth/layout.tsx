c08e0b8b8ba97869ef2e05fb2e5bff80feb18840
HeymerDev <heymermeza11@gmail.com>
2026-09-07 22:19:02 -0500

    modified auth laoyout for add link on logo

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,4 +1,5 @@
 import { Logo } from "@/components/logo/Logo";
+import Link from "next/link";
 
 export default function AuthLayout({
   children,
@@ -10,7 +11,9 @@ export default function AuthLayout({
       <aside className="lg:grid lg:grid-cols-2 lg:min-h-screen">
         <section className="bg-purple-950 flex justify-center lg:bg-auth">
           <div className="py-10 lg:py-20">
-            <Logo />
+            <Link href="/">
+              <Logo />
+            </Link>
           </div>
         </section>
         <section className="p-10 lg:py-28">
